c3d984dbeb1ff5ee183aa07e6d317ae5b4930144
Caike Santos Alves <caikesantosalves@gmail.com>
2026-03-25 23:07:10 -0300

    Adição de uma atividade
    somente uma pois estou na correria pra viajar amanha

---
+++ atividades_python/ex083.py
@@ -0,0 +1,10 @@
+# Crie um programa onde o usuário digite uma expressão qualquer que use parênteses.
+# Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e
+# fechados na ordem correta.
+expressao = input('Digite uma expressão:' )
+aberto = expressao.count('(')
+fechado = expressao.count(')')
+if aberto == fechado:
+    print('A expressao esta certa')
+else:
+    print('A expressão errada')

e9da8e2299bd522896311048813f6e6ab8ab353b
Caike Santos Alves <caikesantosalves@gmail.com>
2026-04-09 22:23:10 -0300

    Adição de Atividade
    coloquei a metade da atividade 84 ja fala quantas pessoas cadastradas e as pessoas mais pesadas.

---
+++ atividades_python/ex084.py
@@ -0,0 +1,30 @@
+#Faça um programa que leia nome e peso de várias pessoas,
+# guardando tudo em uma lista. No final, mostre:
+# A) Quantas pessoas foram cadastradas.
+# B) Uma listagem com as pessoas mais pesadas.
+# C) Uma listagem com as pessoas mais leves.
+pessoas = list()
+pessoa = list()
+quantidade = 0
+peso = 0
+pesada = list()
+leve = list()
+while True:
+    pessoa.append(input('Nome: '))
+    pessoa.append(float(input('Peso: ')))
+    pessoas.append(pessoa[:])
+    pessoa.clear()
+    continuar = input('Deseja continuar? [S/N] ').upper().strip()
+    quantidade += 1
+    if continuar == 'N':
+        break
+for p in pessoas:
+    if p[1] > peso:
+        pesada.clear()
+        peso = p[1]
+        pesada.append(p[0])
+    elif p[1] == peso:
+        pesada.append(p[0])
+
+print(f'Foram cadastradas {quantidade} pessoas.')
+print(f'a pessoa mais pesada e {pesada} com {peso}Kg.')

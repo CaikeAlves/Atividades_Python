c38e160e213a4ad91d3a959ad309563ac6deca2f
Caike Santos Alves <caikesantosalves@gmail.com>
2026-04-17 17:44:42 -0300

    conserto de atividade
    fiz a atividade 85 sem usar listas compostas

--- atividades_python/ex085.py
+++ atividades_python/ex085.py
@@ -1,17 +1,16 @@
 #Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os
 # em uma lista única que mantenha separados os valores pares e ímpares. No final,
 # mostre os valores pares e ímpares em ordem crescente.
-numeros = list()
-pares = list()
-impares = list()
+numeros =[ [], [] ]
 for c in range(1, 8):
     n = int(input(f'Digite o {c}º numero: '))
-    numeros.append(n)
-print(f'Os numeros digitados foram {numeros}')
-for num in numeros:
-    if num % 2 == 0:
-        pares.append(num)
-    else:
-        impares.append(num)
-print(f'Os numeros pares digitados foram {pares}')
-print(f'Os numeros impares digitados foram {impares}')
+    if n % 2 == 0:
+        numeros[0].append(n)
+    else :
+        numeros[1].append(n)
+print(f'Os numeros digitados foram: {numeros}')
+numeros[0].sort()
+numeros[1].sort()
+print(f'Os numeros digitados foram: {numeros}')
+print(f'Os numeros pares digitados foram: {numeros[0]}')
+print(f'Os numeros impares digitados foram: {numeros[1]}')

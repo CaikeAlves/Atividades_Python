1b7ef745da7d057a74b79312a52f41275704d959
Caike Santos Alves <caikesantosalves@gmail.com>
2026-07-23 21:56:19 -0300

    Adição de comentario
    89 comentada pra futuras consultas.

--- atividades_python/ex089.py
+++ atividades_python/ex089.py
@@ -1,20 +1,20 @@
 #Exercício Python 089: Crie um programa que leia nome e duas notas de vários alunos e guarde tudo numa
 # lista composta. No final, mostre um boletim contendo a média de cada um e permita que o usuário
 # possa mostrar as notas de cada aluno individualmente.
-boletim = list()
+boletim = list() #Cria uma lista.
 aluno = list()
-while True:
-    aluno.append(input('Nome: '))
+while True: #Inicia um laço e só para quando coloca break.
+    aluno.append(input('Nome: ')) #faz a pergunta e ja adiciona na lista aluno.
     aluno.append(int(input('Primeira nota: ')))
     aluno.append(int(input('Segunda nota: ')))
-    escolha = input('Deseja adicionar mais um? (N/S)').upper().strip()
-    boletim.append(aluno)
-    aluno = list()
+    escolha = input('Deseja adicionar mais um? (N/S)').upper().strip() #Aqui ele dá um filtrada na resposta colocando ela é maiusculo e tira os espaço antes e dps da resposta.
+    boletim.append(aluno)#add a lista aluno na lista boletim
+    aluno = list()#limpa a lista aluno
     if escolha == 'S':
         pass
     else:
         break
-for posicao,al in enumerate(boletim):
+for posicao,al in enumerate(boletim): #vai pegar indice e valor da lista.
     print(f'Id: {posicao + 1} Aluno: {al[0]} Nota final: {(al[1] + al[2]) / 2}')
 while True:
     escolha = int(input('Deseja consulta a nota de algum aluno  digite o id do aluno caso não queira digite 999.'))

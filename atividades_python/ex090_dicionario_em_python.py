9f1d2d47d53027f0f3133bb426b6b472040ba96a
Caike Santos Alves <caikesantosalves@gmail.com>
2026-07-25 06:45:59 -0300

    Atividade090

--- atividades_python/ex090_dicionario_em_python.py
+++ atividades_python/ex090_dicionario_em_python.py
@@ -1,4 +1,14 @@
 # Faça um programa que leia nome e média de um aluno,
 # guardando também a situação em um dicionário. No final,
 # mostre o conteúdo da estrutura na tela.
-
+nome = input('Digite seu nome: ')
+media = float(input('Digite a sua nota: '))
+aluno = {'nome': nome,'media': media }
+if aluno['media'] >= 7:
+    aluno['situação'] = 'Passou'
+elif aluno['media'] >= 5:
+    aluno['situação'] = 'Recuperação'
+else:
+    aluno['situação'] = 'Reprovou'
+for k,v in aluno.items():
+    print(f'{k} é = {v}')

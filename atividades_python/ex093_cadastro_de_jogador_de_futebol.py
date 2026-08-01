0ec80de070358900405bbb79db1472581844fac5
Caike Santos Alves <caikesantosalves@gmail.com>
2026-07-31 21:31:40 -0300

    atividade 93

--- atividades_python/ex093_cadastro_de_jogador_de_futebol.py
+++ atividades_python/ex093_cadastro_de_jogador_de_futebol.py
@@ -1,5 +1,19 @@
 jogador = dict()
+gols = list()
 jogador['nome'] = input('Digite o nome do jogador: ')
 jogador['jogos'] = int(input('Quantos jogos ele jogou: '))
 for i in range(0,jogador['jogos']):
-    print(f'Quantos gol no seu {i+1}º: ')
+    gol = int(input(f'Quantos gol no seu {i+1}º jogo: '))
+    gols.append(gol)
+jogador['gols'] = gols
+jogador['total'] = sum(jogador['gols'])
+print (20*'-=-')
+print (jogador)
+print (20*'-=-')
+for k,v in jogador.items():
+    print (f'O campo {k} tem o valor {v} ')
+print (20*'-=-')
+print (f'O jogador {jogador["nome"]} jogou {jogador["jogos"]} partidas')
+for i,pt in enumerate(jogador['gols'],start=1):
+    print(f'Na partida {i}, fez {pt} gols')
+print(f'foi um total de {jogador["total"]} gols')

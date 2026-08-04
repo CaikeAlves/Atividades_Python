b3a23aa01d7a3d52116dd2f1458cfbcfa1c81ca3
Caike Alves <caikesantosalves@gmail.com>
2026-08-04 20:51:13 -0300

    Atualização ex095

--- atividades_python/ex095_aprimorando_os_dicionarios.py
+++ atividades_python/ex095_aprimorando_os_dicionarios.py
@@ -1,5 +1,15 @@
 time = list()
 jogador = dict()
 gols = list()
-jogador['nome'] = input('Nome do jogador: ')
-jogador['jogos'] = int(input('Quantas partidas jogadas: '))
+while True:
+    jogador['nome'] = input('Nome do jogador: ')
+    jogador['jogos'] = int(input('Quantas partidas jogadas: '))
+    for c in range(0,jogador['jogos']):
+        g = int(input(f'Quantos gols no {c+1}º jogo: '))
+        gols.append(g)
+    jogador['gols'] = gols
+    time.append(jogador)
+    resposta = input('Deseja cadastrar alguem mais(N/S):').upper()
+    if resposta == 'N':
+        break
+print (time)

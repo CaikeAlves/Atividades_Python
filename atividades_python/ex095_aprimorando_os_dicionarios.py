abb39c90d16c30c0d89fd1e0d3d393189a617971
Caike Santos Alves <caikesantosalves@gmail.com>
2026-08-05 19:47:49 -0300

    Atividade 095 finalizada

--- atividades_python/ex095_aprimorando_os_dicionarios.py
+++ atividades_python/ex095_aprimorando_os_dicionarios.py
@@ -7,9 +7,25 @@ while True:
     for c in range(0,jogador['jogos']):
         g = int(input(f'Quantos gols no {c+1}º jogo: '))
         gols.append(g)
-    jogador['gols'] = gols
-    time.append(jogador)
+    jogador['gols'] = gols [:]
+    jogador['total'] = sum(gols)
+    gols = list()
+    time.append(jogador.copy())
+    jogador = dict()
     resposta = input('Deseja cadastrar alguem mais(N/S):').upper()
     if resposta == 'N':
         break
-print (time)
+print (f'{"Cod":<5}{"Nome":<10}{"Gols":<15}{"total":<5}')
+print ('=='*20)
+for n,atleta in enumerate(time):
+    print(f'{n:<5}{atleta["nome"]:<10}{str(atleta["gols"]):<15}{atleta["total"]:<5}')
+while True:
+    resposta = int(input(f'Deseja ver o levantamento de um jogador? digite o cod dele, caso queira sair digite (999)'))
+    if resposta == 999:
+        break
+    else:
+        for n,atleta in enumerate(time):
+            if n == resposta:
+                print(f'No levantamento do jogador {atleta["nome"]}')
+                for p,gol in enumerate(atleta['gols']):
+                    print(f'No jogo {p+1}º fez {gol}')

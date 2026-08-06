e7e980f69e8fa90ce08fd631c5d810eb892a99d7
Caike Santos Alves <caikesantosalves@gmail.com>
2026-08-06 18:08:47 -0300

    atividade 096

---
+++ atividades_python/ex096_função_calcula_area.py
@@ -0,0 +1,9 @@
+def area(a,b):
+    r = a*b
+    print(f'Uma area com {a}m de largura e {b}m de comprimento é {r}m²')
+
+
+largura = float(input('Digite a largura (m): '))
+comprimento = float(input('Digite o comprimento (m): '))
+
+area(largura,comprimento)
